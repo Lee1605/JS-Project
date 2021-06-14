314497cc5d6e3ba0ac2d8136c8d24637b88814be
Dalia Ashkar <daliaashqar@gmail.com>
2021-06-14 03:33:15 +0300

    first changes

--- Game page/index.js
+++ Game page/index.js
@@ -1,5 +1,20 @@
-let img_11 = document.getElementById('img_11');
-let img_21 = document.getElementById('img_12');
-img_11.addEventListener('click',(event)=>{
-    console.log("aaaa");
-});
+let image1 = document.getElementsByClassName("img_1");
+let image2 = document.getElementsByClassName("img_2");
+
+
+
+
+for(let i=0; i<image1.length;i++){
+    image1[i].addEventListener('click',(event)=>{
+        image1[i].style.zIndex = '-1';
+        storedNum.push(i);
+        if(storedNum.length == 2){
+            if(image2[storedNum[0]].getAttribute("class") != image2[storedNum[1]].getAttribute("class")){
+                image1[storedNum[0]].style.zIndex = '1';
+                image1[storedNum[1]].style.zIndex = '1';
+            }
+            storedNum=[];
+        }
+    })
+}
+
