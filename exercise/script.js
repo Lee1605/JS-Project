08a4fe7815a68567932140d0b29e11af5e03d8a0
Lee Meloul <leemalul555@gmail.com>
2021-06-30 21:09:36 +0200

    shuffle function

--- exercise/script.js
+++ exercise/script.js
@@ -15,6 +15,14 @@ let arr3 = [];
 let arr4 = [];
 let sum1 = 0;
 
+let params = window.location.search;
+console.log(params);
+
+let params1 = new URLSearchParams(params);
+console.log(params1.get('Full-name'));
+let fname = params1.get('Full-name');
+score.innerHTML = score.innerHTML + ' ' + fname;
+
 
 
 
@@ -41,7 +49,7 @@ for(let i = 0;i<memory.length; i++) {
                 setTimeout(function(){cards[value].classList.add('stay')},1000)})
             arr2.push(i);
             console.log(arr2);
-            score.innerHTML = ('you found ' + arr2.length + ' out of 6 pairs');
+            score.innerHTML = ('You found ' + arr2.length + ' out of 6 pairs');
             if(arr3.length>=2) {
                 arr4.push(10);
                 sum1 = 0;
@@ -60,7 +68,7 @@ for(let i = 0;i<memory.length; i++) {
             arr3 = [];
             console.log('not match');
             for(let i = 0;i<cards.length;i++){
-            setTimeout(function(){cards[i].classList.remove('flip')},1300)};
+            setTimeout(function(){cards[i].classList.remove('flip')},1200)};
         }
         
         arr = [];
