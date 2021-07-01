951ee9aacaea20ad7dda618a0ac51e3178d5a059
Daliaash6 <82088424+Daliaash6@users.noreply.github.com>
2021-07-01 08:11:52 +0300

    Add files via upload

---
+++ Landing-page/script.js
@@ -0,0 +1,5 @@
+let button = document.getElementById('myButton');
+let playerName = document.getElementById('playerName');
+console.log(playerName.value);
+let playerEmail = document.getElementById('playerEmail');
+console.log(playerEmail.value);
