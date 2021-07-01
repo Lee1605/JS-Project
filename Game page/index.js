89efd7f943c7ed1f65c23c59bd84e45ca2a11848
Daliaash6 <82088424+Daliaash6@users.noreply.github.com>
2021-07-01 08:09:22 +0300

    Add files via upload

--- Game page/index.js
+++ Game page/index.js
@@ -1,36 +1,62 @@
-let image1 = document.getElementsByClassName("img_1");
-let image2 = document.getElementsByClassName("img_2");
+let param = new URLSearchParams( window.location.search );
+let FullName = param.get('name');
+let Email = param.get('Email');
+const score = document.getElementById('score');
+score.innerHTML = 'Hello ' + FullName;
 
+let has_flipped = false;
+const memory = document.querySelectorAll('.image')
+const cards = document.querySelectorAll('.img1');
+const body = document.getElementById('body');
+const after = document.getElementById('after');
+const pop = document.getElementById('pop');
+const points = document.getElementById('points');
+let sum = 0;
+points.innerHTML = 'Your points : ' + 0;
+let arr = [];
+let array = [];
+let x;
+let y;
+let has = false;
 
-let storedNum = [];
-for(let i=0; i<image1.length;i++){
-    image1[i].addEventListener('click',(event)=>{
-        image1[i].style.zIndex = '-1';
-        storedNum.push(i);
-        console.log(storedNum);
-        if(storedNum.length > 1){
-            if( image2[storedNum[0]].getAttribute("class") != image2[storedNum[1]].getAttribute("class") ){
-                storedNum.forEach( (value, index) => {
-                    setTimeout( function() {
-                        image1[value].style.zIndex = '1';
-                    },2000);
-                });
-            }
-            setTimeout(function(){storedNum = []},2100);
+for(let i = 0;i<memory.length; i++) {
+    memory[i].addEventListener('click',(event)=>{
+        if(!has_flipped) {
+            x=i;
+            has_flipped = true;
+            arr.push(flip(i));
         }
-    })
+        else {
+            y=i;
+            has_flipped = false;
+            arr.push(flip(i));
+            if(arr[0]!=arr[1]){
+                unFlip(x);
+                unFlip(y);
+            }
+            else{
+                array.push(arr[0]);
+                sum = sum + 5;
+                points.innerHTML = 'your points : ' + sum;
+                if(array.length===6) {
+                    body.style.backgroundColor = 'rgba(0,0,0,0.6)';
+                    after.style.opacity = '0.5';
+                    pop.style.display = 'initial';
+                }
+            }
+            arr = [];
+        } 
+    });
+    has_flipped = false;
 }
 
+function flip(j){
+    cards[j].classList.add('flip');
+    return memory[j].getAttribute('class');
+}
 
-for(let i=0; i<image1.length;i++){
-    image1[i].addEventListener('mouseover',(event)=>{
-        if(image1[i].style.zIndex == '-1'){
-            image1[i].style.transform = '';
-        }
-        else{
-            image1[i].style.transform = 'scale(1.1) rotate(2deg)'; 
-        }
-    })
+function unFlip(j){
+    setTimeout(function(){cards[j].classList.remove('flip')},300);       
 }
 
 
