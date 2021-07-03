19b725e623c3c5d2d6a276b475d24575b2ea54e1
Daliaash6 <82088424+Daliaash6@users.noreply.github.com>
2021-07-04 01:17:02 +0300

    Add files via upload

--- Game page/index.js
+++ Game page/index.js
@@ -11,6 +11,8 @@ const body = document.getElementById('body');
 const after = document.getElementById('after');
 const pop = document.getElementById('pop');
 const points = document.getElementById('points');
+const again = document.getElementById('again');
+const scores = document.getElementById('scores');
 let sum = 0;
 points.innerHTML = 'Your points : ' + 0;
 let arr = [];
@@ -18,6 +20,40 @@ let array = [];
 let x;
 let y;
 let has = false;
+let finish = document.getElementById('finish');
+
+// DOM integration
+stopperDisplay = document.getElementById("stopper");
+// initializing
+seconds = 0;
+minutes = 0;
+/* function logic: every second seconds variable increasing by 1 until 60.
+when seconds variable reach to 60, seconds variable initializing and minutes variabale increasing by 1 */
+function stopper() {
+    seconds++;
+    if (seconds / 60 === 1) {
+        minutes++;
+        seconds = 0;
+    }
+    if (array.length === 6) {
+        return;
+    }
+    if(minutes<10 & seconds<10){
+        stopperDisplay.innerHTML = "0" + minutes + ":" + "0" + seconds;
+    }
+    else{
+        if(minutes<10){
+            stopperDisplay.innerHTML = "0" + minutes + ":" + seconds;
+        }
+        if(seconds<10){
+            stopperDisplay.innerHTML = minutes + ":" + "0" + seconds;
+        }
+        else{
+            topperDisplay.innerHTML = minutes + ":" + seconds;
+        }
+    }
+}
+window.setInterval(stopper, 1000);
 
 for(let i = 0;i<memory.length; i++) {
     memory[i].addEventListener('click',(event)=>{
@@ -41,7 +77,9 @@ for(let i = 0;i<memory.length; i++) {
                 if(array.length===6) {
                     body.style.backgroundColor = 'rgba(0,0,0,0.6)';
                     after.style.opacity = '0.5';
+                    finish.innerHTML = 'Well Done ' + FullName;
                     pop.style.display = 'initial';
+                    scores.childNodes[0].href="../Ending page/index.html?name=" + FullName + "&Email=" + Email + "&points=" + sum + "&time=" + ( minutes + seconds/60);
                 }
             }
             arr = [];
@@ -59,5 +97,11 @@ function unFlip(j){
     setTimeout(function(){cards[j].classList.remove('flip')},300);       
 }
 
+(function shuffle() {
+    memory.forEach(card=> {
+        let random_pos = Math.floor(Math.random()*12);
+        card.style.order = random_pos;
+    })
+}) ();
 
-
+again.childNodes[0].href = "index.html?name=" + FullName + "&Email=" + Email; 
