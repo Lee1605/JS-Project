8e85f6f082c439ad33c594bc23018a67370113c2
Daliaash6 <82088424+Daliaash6@users.noreply.github.com>
2021-07-04 13:26:29 +0300

    Add files via upload

--- Ending page/scripts.js
+++ Ending page/scripts.js
@@ -3,8 +3,8 @@ const playerNameAccToTime = document.getElementById("playerNameAccToTime");
 const playerPoints = document.getElementById("playerPoints");
 const playerTime = document.getElementById("playerTime");
 
-let param = new URLSearchParams( window.location.search );
-playerName.innerHTML = param.get('name');
-playerNameAccToTime.innerHTML = param.get('name');
-playerPoints.innerHTML = param.get('points');
-playerTime.innerHTML = param.get('time') + " min";
+let param = new URLSearchParams( window.location.search );//Get the parameters on the url
+playerName.innerHTML = param.get('name');//Get the value of the parameter name and put it in the points table 
+playerNameAccToTime.innerHTML = param.get('name');//Get the value of the parameter name and put it in the time table 
+playerPoints.innerHTML = param.get('points');//Get the value of the parameter points and put it in the points table 
+playerTime.innerHTML = param.get('time') + " min";//Get the value of the parameter time and put it in the time table 
