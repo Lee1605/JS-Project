5c8f799e71669e972c3098fb56bea24cbf40779d
Dalia Ashkar <daliaashqar@gmail.com>
2021-07-04 14:46:04 +0300

    changes

--- Landing-page/script.js
+++
@@ -1,5 +0,0 @@
-let button = document.getElementById('myButton');
-let playerName = document.getElementById('playerName');
-console.log(playerName.value);
-let playerEmail = document.getElementById('playerEmail');
-console.log(playerEmail.value);
