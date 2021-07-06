6c0a1942596463d384245e05fa0d1f9b9351c021
Daliaash6 <82088424+Daliaash6@users.noreply.github.com>
2021-07-06 15:26:08 +0300

    Add files via upload

--- Ending page/scripts.js
+++ Ending page/scripts.js
@@ -12,33 +12,39 @@ playerTime.innerHTML = param.get('time') + " min";//Get the value of the paramet
 let whatsapp = document.getElementById('whatsapp');
 whatsapp.href = 'https://api.whatsapp.com/send?text=My Points' + param.get('points')+'My time'+param.get('time');
 
-// const tds = document.querySelectorAll('.td');
-// let arr = [];
-// function compareNumbers(a, b)
-// {
-//     return a - b;
-// }
+let table = document.querySelectorAll('table');
+let rows = table.rows;
+let targetedPerson = table[0].rows[4];
+for(let i= 1 ; i < table[0].rows.length-1; i++){
+    let x = targetedPerson;
+    let y = table[0].rows[i];
+    if(x.innerText != y.innerText){
+        if(Number(x.lastElementChild.innerHTML) >= Number(y.lastElementChild.innerHTML)){
+            exchange(x,y);
+        }
+    }
+}
 
-// arr.sort(compareNumbers);
-// console.log(arr);
+// y = 4;
+targetedPerson = table[1].rows[4];  
+console.log(targetedPerson);
+for(let i= 1 ; i < table[0].rows.length-1; i++){
+    console.log(1);
+    x = targetedPerson;
+    y = table[1].rows[i];
+    if(x.innerText != y.innerText){
+        if(parseInt(x.lastElementChild.innerHTML) <= parseInt(y.lastElementChild.innerHTML)){
+            exchange(x,y);
+        }
+    }
+}
 
-// const trs = document.querySelectorAll('.tr');
-// let newarr = [];
-// for(let i = 0;i<trs.length;i++) {
-//     arr.push(trs[i].innerHTML);
-//     let point1 = (trs[0].lastElementChild.innerHTML);
-//     let point2 = (trs[1].lastElementChild.innerHTML);
-//     let point3 = (trs[2].lastElementChild.innerHTML);
-//     let point4 = (trs[3].lastElementChild.innerHTML);
-
-//     console.log(point1,point2,point3,point4);
-//     if(point4>point1&&point4>=point2&&point4<point3){
-//         newarr = [arr[2],arr[3],arr[1],arr[0]];
-//     }
-
-
-// }
-// console.log(newarr);
+function exchange(x,y){
+    let z;
+    z = y.innerHTML;
+    y.innerHTML = x.innerHTML;
+    x.innerHTML = z;  
+}
 
 
 
