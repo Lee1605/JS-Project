c2728d66fbd40a4b60728aac6744f8fa2d2a920d
Lee Meloul <leemalul555@gmail.com>
2021-07-07 14:22:53 +0300

    rounding time

--- Ending page/scripts.js
+++ Ending page/scripts.js
@@ -3,11 +3,15 @@ const playerNameAccToTime = document.getElementById("playerNameAccToTime");
 const playerPoints = document.getElementById("playerPoints");
 const playerTime = document.getElementById("playerTime");
 
+
 let param = new URLSearchParams( window.location.search );//Get the parameters on the url
+let time = param.get('time');
+time = Number(time);
 playerName.innerHTML = param.get('name');//Get the value of the parameter name and put it in the points table 
 playerNameAccToTime.innerHTML = param.get('name');//Get the value of the parameter name and put it in the time table 
 playerPoints.innerHTML = param.get('points');//Get the value of the parameter points and put it in the points table 
-playerTime.innerHTML = param.get('time') + " min";//Get the value of the parameter time and put it in the time table
+playerTime.innerHTML = time.toFixed(1) + " min";//Get the value of the parameter time and put it in the time table
+
 
 let whatsapp = document.getElementById('whatsapp');
 whatsapp.href = 'https://api.whatsapp.com/send?text=I wanted to share with you my game result ! '+param.get('name')+' - '+param.get('Email')+', I got '+param.get('points')+' points in '+param.get('time') +' minutes.';
